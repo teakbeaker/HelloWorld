42200f32b64982e9a9737aac56df014d68e4c42e
teakbeaker <47350237+teakbeaker@users.noreply.github.com>
2019-02-05 14:10:27 +0000

    Add files via upload

---
+++ app5.js
@@ -0,0 +1,25 @@
+// app.js
+var express = require('express');
+var $ = require('jQuery');
+
+var app = express();
+
+var server = require('http').createServer(app);
+
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//-- local
+var routes = require('./routes.js')(app);
+
+//-- 
+app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname + '/images'));
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/js'));
+
+server.listen(8080, function(){
+	console.log('Server started on 8080...dddd');
+});
